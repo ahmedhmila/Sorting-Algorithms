5f88000fdba77a200f807fd4426d121987a9b8c1
Ahmed Hmila <74432440+ahmedhmila@users.noreply.github.com>
2023-09-25 19:19:21 +0100

    Update Sorting.js
    Fixed a display issue not letting some rectangles appear on large screens

--- src/js/Sorting.js
+++ src/js/Sorting.js
@@ -43,12 +43,24 @@ function fill(nb) {
 //verifi w fill array
 function VerifandFill() {
 	var f = document.getElementById("numE").value;
+	const rec_container=document.getElementById("rectangle-container");
 	if (!chiffre(f)) {
 		alert("Enter a number between 2 & 99");
 	}
+	document.addEventListener("DOMContentLoaded", function() {
+
+	if (f>34){
+		rec_container.style.justifyContent = "left";
+	}
+	if (f<34){
+		rec_container.style.justifyContent = "center";
+	}});
+
+	
 	let array = fill(f);
 	return array;
 }
+
 
 async function bubbleSort(arr) {
 	let isSortingComplete = false;
@@ -733,3 +745,5 @@ function heapify(arr, n, i){
 	title.innerHTML = `${ty} Sort`;
 	textContent.innerHTML = isAlgorithmShown ? `<h2>Algorithm</h2><pre>${algorithm}</pre>` : `<h2>Explanation</h2>${explanation}`;
 }
+
+
